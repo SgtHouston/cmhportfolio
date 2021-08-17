3d6c0defc97d53fbd343fb79b7755e1df0c9b042
Chris Houston <chrisbcoding247@gmail.com>
2021-08-17 18:02:21 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+// Step 1 - npm install react-router-dom and import 
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Step 1
+    <Router >
+      <div className="App">
+        Hello World
+      </div>
+    </Router>
   );
 }
 
